e5e94d31a75a1986f34fa3d8ac7203bc1b215db9
MedveMarci <martonmende@gmail.com>
2025-11-10 15:54:49 +0100

    Added option UsePickup (until NW fix it it's always false)

--- Scp1162/Plugin.cs
+++ Scp1162/Plugin.cs
@@ -8,13 +8,14 @@ namespace SCP1162;
 public class Plugin : Plugin<Config>
 {
     public static Plugin Instance;
+    public string githubRepo = "MedveMarci/SCP-1162";
     public override string Description => "A mysterious hole that gives random items.";
     public override string Name => "SCP-1162";
     public override string Author => "MedveMarci";
-    public override Version Version { get; } = new(1, 1, 0);
+    public override Version Version { get; } = new(1, 2, 0);
     public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
     private EventHandler EventHandler { get; } = new();
-    public string githubRepo = "MedveMarci/SCP-1162";
+
     public override void Enable()
     {
         Instance = this;
